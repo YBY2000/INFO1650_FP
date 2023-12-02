a0243a9e36a991cabfc93f0bbaeea08c8650ddbf
jqkim66 <jiang.qian@northeastern.edu>
2023-12-02 01:04:42 -0500

    update style of login

--- Frontend/src/pages/login/index.jsx
+++ Frontend/src/pages/login/index.jsx
@@ -1,91 +1,95 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
+import './index.scss';
 import { useNavigate } from 'react-router-dom';
+import useRequest from '../../hooks/useRequest';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [users, setUsers] = useState([]);
-  const [formValidated, setFormValidated] = useState(false);
   const navigate = useNavigate();
-
-  // 模拟从 API 获取用户数据
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch("../../mock/user.json");
-        const usersData = await response.json();
-        setUsers(usersData);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    fetchUsers();
-  }, []);
+  const { request } = useRequest('/auth/login', { method: 'POST' });
+  const formRef = useRef(null); // 创建一个 ref 来引用表单
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    setFormValidated(true);
 
-    if (e.currentTarget.checkValidity() === false) {
-      e.stopPropagation();
-    } else {
-      // 用户验证逻辑
-      const user = users.find(user => user.account === email && user.passcode === password);
-      console.log(users);
+    // 使用 ref 检查表单的有效性
+    if (formRef.current && !formRef.current.checkValidity()) {
+      // 如果表单无效，显示错误消息
+      formRef.current.classList.add('was-validated'); // Bootstrap 类用于显示验证错误
+      return;
+    }
 
-      if (user) {
-        localStorage.setItem("user", JSON.stringify(user));
-        navigate('/about'); // 用 navigate 替代 window.location.href
+    // 确保 email 和 password 不为空
+    if (!email || !password) {
+      alert('Please enter email and password');
+      return;
+    }
+
+    // 提交请求
+    const requestBody = { email, password };
+    try {
+      const data = await request(requestBody);
+      if (data.success) {
+        alert('Login successful!');
+        localStorage.setItem('token', data.data.token);
+        navigate('/home');
       } else {
-        // 显示登录错误信息
-        alert('Invalid account or password.'); // 这里用 alert 示意，可以替换为更复杂的错误消息显示逻辑
+        alert(data.message);
       }
+    } catch (error) {
+      console.error('Error:', error);
     }
   };
 
-  const handleBack = () => {
-    navigate('/home'); // 用 navigate 替代 window.location.href
-  };
-
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <h2>Login</h2>
-          <form 
-            className={`needs-validation ${formValidated ? 'was-validated' : ''}`} 
-            noValidate
-            onSubmit={handleLogin}
-          >
+    <>
+      <div id="liveAlertPlaceholder"></div>
+
+      <div id="backButtonContainer" className="backButtonContainer">
+        <button type="button" className="btn btn-secondary" onClick={() => navigate(-1)}>
+          Back
+        </button>
+      </div>
+
+      <div className="loginContainer">
+        <div className="left">
+          <h1>Login now</h1>
+          <h1>To start your journey!</h1>
+          <form className="needs-validation" noValidate onSubmit={handleLogin} ref={formRef}>
             <div className="mb-3">
-              <label htmlFor="inputEmail3" className="form-label">Email</label>
-              <input 
-                type="email" 
-                className="form-control" 
+              <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
                 id="inputEmail3"
+                placeholder="xxx@xx.xxx"
+                required
+                pattern="^[a-zA-Z0-9._\\u002D]+@[a-zA-Z0-9._\\u002D]+\.[a-zA-Z]+"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                required 
               />
             </div>
             <div className="mb-3">
-              <label htmlFor="inputPassword3" className="form-label">Password</label>
-              <input 
-                type="password" 
-                className="form-control" 
+              <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
                 id="inputPassword3"
+                placeholder="At least 6 characters"
+                required
+                minLength="6"
+                maxLength="50"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                required 
               />
             </div>
             <button type="submit" className="btn btn-primary">Log in</button>
-            <button type="button" className="btn btn-secondary" onClick={handleBack}>Back</button>
           </form>
         </div>
+        <img src="../../../business-meeting.png" alt="Business Meeting" />
       </div>
-    </div>
+    </>
   );
 };
 
