90a60db279392be2560ff318c190ba334d3249e7
jqkim66 <jiang.qian@northeastern.edu>
2023-12-04 19:38:16 -0500

    update sytle for sign up page

--- Frontend/src/pages/signUp/index.jsx
+++ Frontend/src/pages/signUp/index.jsx
@@ -11,6 +11,7 @@ import {
     message
 } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
+import './index.scss';
 
 const { TextArea } = Input;
 
@@ -96,41 +97,41 @@ const RegistrationPage = () => {
         if (!validateEmail(formData.email)) {
             message.error('Invalid email address');
             return;
-          }
-          if (!validatePassword(formData.password)) {
+        }
+        if (!validatePassword(formData.password)) {
             message.error('The password is too weak. The password contains at least one digit, one lowercase letter, and one uppercase letter, and must contain at least eight characters');
             return;
-          }
-          if (!validateName(formData.firstName)) {
+        }
+        if (!validateName(formData.firstName)) {
             message.error('Invalid first name');
             return;
-          }
-          if (!validateName(formData.lastName)) {
+        }
+        if (!validateName(formData.lastName)) {
             message.error('Invalid last name');
             return;
-          }
-          if (!formData.gender) {
+        }
+        if (!formData.gender) {
             message.error('Please select a gender');
             return;
-          }
+        }
 
-          if (!formData.country) {
+        if (!formData.country) {
             message.error('Please select a country');
             return;
-          }
-          
-          if (!formData.description || !validateDescription(formData.description)) {
+        }
+
+        if (!formData.description || !validateDescription(formData.description)) {
             message.error('Invalid description, description should be 1 to 200 letters');
             return;
-          }
-          if (!validateAge(formData.age)) {
+        }
+        if (!validateAge(formData.age)) {
             message.error('Must be between 15 and 99 years old');
             return;
-          }
-          if (!validateInterests(formData.interest)) {
+        }
+        if (!validateInterests(formData.interest)) {
             message.error('The selected interest is invalid');
             return;
-          }
+        }
         try {
             const response = await fetch('http://localhost:3000/api/user/create', {
                 method: 'POST',
@@ -155,66 +156,68 @@ const RegistrationPage = () => {
 
 
     return (
-        <Form
-            form={form}
-            layout="horizontal"
-            onFinish={handleSubmit}
-            labelCol={{ span: 4 }}
-            wrapperCol={{ span: 14 }}
-            style={{ maxWidth: 600 }}
-        >
-            <Form.Item label="Email" name="email">
-                <Input onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
-            </Form.Item>
-            <Form.Item label="Password" name="password">
-                <Input.Password onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
-            </Form.Item>
-            {/* ...其他表单元素... */}
-            <Form.Item label="First Name" name="firstName">
-                <Input onChange={(e) => setFormData({ ...formData, firstName: e.target.value })} />
-            </Form.Item>
-            <Form.Item label="Last Name" name="lastName">
-                <Input onChange={(e) => setFormData({ ...formData, lastName: e.target.value })} />
-            </Form.Item>
-            <Form.Item label="Gender" name="gender">
-                <Radio.Group onChange={(e) => setFormData({ ...formData, gender: e.target.value })}>
-                    <Radio value={0}>Female</Radio>
-                    <Radio value={1}>Male</Radio>
-                </Radio.Group>
-            </Form.Item>
-            <Form.Item label="Country" name="country">
-                <Select onChange={(value) => setFormData({ ...formData, country: value })}>
-                    {countries.map(country => (
-                        <Select.Option key={country.id} value={country.id}>{country.name}</Select.Option>
-                    ))}
-                </Select>
-            </Form.Item>
-            {/* ...其他表单元素... */}
-            <Form.Item label="Description" name="description">
-                <TextArea rows={4} onChange={(e) => setFormData({ ...formData, description: e.target.value })} />
-            </Form.Item>
-            <Form.Item label="Avatar" name="avatar">
-                <Upload
-                    listType="picture-card"
-                    fileList={fileList}
-                    onChange={handleUploadChange}
-                    beforeUpload={() => false}
-                >
-                    {fileList.length < 1 && <PlusOutlined />}
-                </Upload>
-            </Form.Item>
-            <Form.Item label="Age" name="age">
-                <Input type="number" onChange={(e) => setFormData({ ...formData, age: e.target.value })} />
-            </Form.Item>
-            <Form.Item label="Interests" name="interest">
-                <Checkbox.Group options={interestsOptions} onChange={onInterestChange} />
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 4 }}>
-                <Button type="primary" htmlType="submit">
-                    Register
-                </Button>
-            </Form.Item>
-        </Form>
+        <div className="formContainer">
+            <Form
+                form={form}
+                layout="horizontal"
+                onFinish={handleSubmit}
+                labelCol={{ span: 4 }}
+                wrapperCol={{ span: 14 }}
+                style={{ maxWidth: 600 }}
+            >
+                <Form.Item label="Email" name="email">
+                    <Input onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+                </Form.Item>
+                <Form.Item label="Password" name="password">
+                    <Input.Password onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+                </Form.Item>
+                {/* ...其他表单元素... */}
+                <Form.Item label="First Name" name="firstName">
+                    <Input onChange={(e) => setFormData({ ...formData, firstName: e.target.value })} />
+                </Form.Item>
+                <Form.Item label="Last Name" name="lastName">
+                    <Input onChange={(e) => setFormData({ ...formData, lastName: e.target.value })} />
+                </Form.Item>
+                <Form.Item label="Gender" name="gender">
+                    <Radio.Group onChange={(e) => setFormData({ ...formData, gender: e.target.value })}>
+                        <Radio value={0}>Female</Radio>
+                        <Radio value={1}>Male</Radio>
+                    </Radio.Group>
+                </Form.Item>
+                <Form.Item label="Country" name="country">
+                    <Select onChange={(value) => setFormData({ ...formData, country: value })}>
+                        {countries.map(country => (
+                            <Select.Option key={country.id} value={country.id}>{country.name}</Select.Option>
+                        ))}
+                    </Select>
+                </Form.Item>
+                {/* ...其他表单元素... */}
+                <Form.Item label="Description" name="description">
+                    <TextArea rows={4} onChange={(e) => setFormData({ ...formData, description: e.target.value })} />
+                </Form.Item>
+                <Form.Item label="Avatar" name="avatar">
+                    <Upload
+                        listType="picture-card"
+                        fileList={fileList}
+                        onChange={handleUploadChange}
+                        beforeUpload={() => false}
+                    >
+                        {fileList.length < 1 && <PlusOutlined />}
+                    </Upload>
+                </Form.Item>
+                <Form.Item label="Age" name="age">
+                    <Input type="number" onChange={(e) => setFormData({ ...formData, age: e.target.value })} />
+                </Form.Item>
+                <Form.Item label="Interests" name="interest">
+                    <Checkbox.Group options={interestsOptions} onChange={onInterestChange} />
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 4 }}>
+                    <Button type="primary" htmlType="submit">
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
     );
 };
 
