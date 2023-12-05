50127992e5723b9c7f12962c595b78314c830bbb
YBY2000 <ye.boy@northeastern.edu>
2023-12-05 18:09:08 -0500

    tried placeholder

--- Frontend/src/components/card-list/card-list.component.js
+++ Frontend/src/components/card-list/card-list.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './card-list.style.css'; // Import the CSS file for styling
+import { Spinner, Placeholder } from 'react-bootstrap';
 
 
 class Cards extends Component {
@@ -14,25 +15,68 @@ class Cards extends Component {
     }
 
     pageSwitch = (attractionId) => {
-      // Replace with your page switching logic
-      window.location.href = '/detail?id=' + attractionId;
+        // Replace with your page switching logic
+        window.location.href = '/detail?id=' + attractionId;
     };
 
     render() {
         const { attractions } = this.props;
         const { getImg } = this;
         return (
-            <div className='card-list'>
-                {attractions.map((attractions) => (
-                    <div className='card-container' key={attractions.id}>
-                        <img className="card-img" alt={`attraction ${attractions.name}`} src={getImg(attractions)} />
-                        <div class="card-intro-area">
-                            <p className='card-title'>{attractions.name}</p>
-                            <p className='card-text'>{attractions.description}</p>
+            <>
+                <div className='card-list'>
+                    {attractions.map((attractions) => (
+                        <div>
+                            {/* {isLazyLoading ? (
+                                <div className='card-container' key={attractions.id}>
+                                    <img className="card-img" alt={`attraction ${attractions.name}`} src={getImg(attractions)} />
+                                    <div class="card-intro-area">
+                                        <p className='card-title'>{attractions.name}</p>
+                                        <p className='card-text'>{attractions.description}</p>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className='card-container' key={attractions.id}>
+                                    <div className="card-img"><Spinner animation="grow" /></div>
+                                    <div class="card-intro-area">
+                                        <Placeholder className='card-title' animation="glow">
+                                            <Placeholder xs={6} />
+                                        </Placeholder>
+                                        <Placeholder className='card-text' animation="glow">
+                                            <Placeholder style={{ margin: '30px 0 0 0' }} xs={7} /> <Placeholder style={{ margin: '30px 0 0 0' }} xs={4} /> <Placeholder xs={4} />{' '}
+                                            <Placeholder xs={6} /> <Placeholder xs={8} />
+                                        </Placeholder>
+                                    </div>
+                                </div>
+                            )} */}
+
+
+                            {/* <div className='card-container' key={attractions.id}>
+                                <img className="card-img" alt={`attraction ${attractions.name}`} src={getImg(attractions)} />
+                                <div class="card-intro-area">
+                                    <p className='card-title'>{attractions.name}</p>
+                                    <p className='card-text'>{attractions.description}</p>
+                                </div>
+                            </div>
+                            <div className='card-container' key={attractions.id}>
+                                <div className="card-img"><Spinner animation="grow" /></div>
+                                <div class="card-intro-area">
+                                    <Placeholder className='card-title' animation="glow">
+                                        <Placeholder xs={6} />
+                                    </Placeholder>
+                                    <Placeholder className='card-text' animation="glow">
+                                        <Placeholder style={{ margin: '30px 0 0 0' }} xs={7} /> <Placeholder style={{ margin: '30px 0 0 0' }} xs={4} /> <Placeholder xs={4} />{' '}
+                                        <Placeholder xs={6} /> <Placeholder xs={8} />
+                                    </Placeholder>
+                                </div>
+                            </div> */}
+
                         </div>
-                    </div>
-                ))}
-            </div>
+
+                    ))}
+                </div>
+            </>
+
         );
     }
 
