e3103ab57ef8277879f4eba478196fbada57c0c9
YBY2000 <ye.boy@northeastern.edu>
2023-12-05 18:09:35 -0500

    fix bug

--- Frontend/src/components/card-list/card-list.component.js
+++ Frontend/src/components/card-list/card-list.component.js
@@ -51,25 +51,13 @@ class Cards extends Component {
                             )} */}
 
 
-                            {/* <div className='card-container' key={attractions.id}>
+                            <div className='card-container' key={attractions.id}>
                                 <img className="card-img" alt={`attraction ${attractions.name}`} src={getImg(attractions)} />
                                 <div class="card-intro-area">
                                     <p className='card-title'>{attractions.name}</p>
                                     <p className='card-text'>{attractions.description}</p>
                                 </div>
                             </div>
-                            <div className='card-container' key={attractions.id}>
-                                <div className="card-img"><Spinner animation="grow" /></div>
-                                <div class="card-intro-area">
-                                    <Placeholder className='card-title' animation="glow">
-                                        <Placeholder xs={6} />
-                                    </Placeholder>
-                                    <Placeholder className='card-text' animation="glow">
-                                        <Placeholder style={{ margin: '30px 0 0 0' }} xs={7} /> <Placeholder style={{ margin: '30px 0 0 0' }} xs={4} /> <Placeholder xs={4} />{' '}
-                                        <Placeholder xs={6} /> <Placeholder xs={8} />
-                                    </Placeholder>
-                                </div>
-                            </div> */}
 
                         </div>
 
